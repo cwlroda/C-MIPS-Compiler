6182bd6b3166bd18ce25c4b50e8441ca3c94631f
Wei Loon <47574881+cwlroda@users.noreply.github.com>
2020-02-18 19:48:23 +0000

    Updates

--- include/ast.hpp
+++ include/ast.hpp
@@ -1,7 +1,10 @@
 #ifndef AST_HPP
 #define AST_HPP
 
+#include "ast/ast_node.hpp"
 #include "ast/ast_decl.hpp"
 #include "ast/ast_op.hpp"
+
+extern const Node *parseAST(const char file[]);
 
 #endif
---
+++ include/ast/ast_context.hpp
@@ -0,0 +1,20 @@
+#ifndef AST_CONTEXT_HPP
+#define AST_CONTEXT_HPP
+
+#include <string>
+#include <vector>
+#include <map>
+
+class Context{
+    public:
+        Context();
+
+    private:
+        std::vector<std::map<std::string, std::string> > scope;
+        std::vector<int> params;
+        std::map<std::string, int> bindings;
+        int stack[];
+
+};
+
+#endif
