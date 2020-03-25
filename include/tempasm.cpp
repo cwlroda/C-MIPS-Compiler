4a0bd34da7ee1232d1f108df3f3eb96c26f546cc
marcus418 <56596363+marcus418@users.noreply.github.com>
2020-03-25 20:38:41 +0800

    Update tempasm.cpp

--- include/tempasm.cpp
+++ include/tempasm.cpp
@@ -50,8 +50,22 @@ inline void ExternalDeclaration::print_asm(std::ofstream& out){
     if(decl != NULL){
         context.is_GlobalVar = true;
 
-        out<< std::endl;
+        out << std::endl;
         decl->print_asm(out);
+        out << "\t.data" << std::endl;
+        out << "\t.globl\t" << context.GlobalDirectDeclarator << std::endl;
+        out << "\t.align\t2" << std::endl;
+        out << "\t.type\t" << context.GlobalDirectDeclarator << ", @object" << std::endl;
+        out << "\t.size\t" << context.GlobalDirectDeclarator << ", ";
+
+        if(context.what_typeSpec == "char"){
+            out << "1";
+        }
+        else{
+            out << "4";
+        }
+        out << std::endl;
+
         out << context.GlobalDirectDeclarator << ":" << std::endl;
         if(context.what_typeSpec == "char"){
             out << "\t.byte\t";
@@ -60,31 +74,80 @@ inline void ExternalDeclaration::print_asm(std::ofstream& out){
             out << "\t.word\t";
         }
         out << context.GlobalVarNum << std::endl;
+
+        context.is_GlobalVar = false;
+        context.what_typeSpec = "0";
+        context.GlobalDirectDeclarator = "0";
     }
 
-    if(func_def != NULL){
+    else if(func_def != NULL){
+        context.in_func = true;
+        out << std::endl;
+        func_def->print_asm(out);
         
-    } 
+        context.FuncName = "0";
+        context.in_func = false;
+    }
+
+    out << std::endl;
+}
+
+inline void FunctionDefinition::print_asm(std::ofstream& out){
+    if(decl_spec != NULL){
+        decl_spec->print_asm(out);
+    }
+    declr->print_asm(out);
+
+    out << "\t.text" << std::endl;
+    out << "\t.align\t2" << std::endl;
+    out << "\t.globl\t" << context.FuncName << std::endl;
+    out << "\t.set\tnomips16" << std::endl;
+    out << "\t.set\tnomicromips" << std::endl;
+    out << "\t.ent\t" << context.FuncName << std::endl;
+    out << "\t.type\t" << context.FuncName << ", @function" << std::endl;
+
+    out << context.FuncName << ":" << std::endl;
+
+    out << "\taddiu\t$sp,$sp,-48" << std::endl;
+    out << "\tsw\t$31,44($sp)" << std::endl;
+    out << "\tsw\t$fp,40($sp)" << std::endl;
+    out << "\tmove $fp,$sp" << std::endl;
+
+
+    if(decl_list != NULL){
+        decl_list->print_asm(out);
+    }
+
+    comp_state -> print_asm(out);
+
+    out << "\tmove\t$sp,$fp" << std::endl;
+    out << "\tlw\t$31,44($sp)" << std::endl;
+    out << "\tlw\t$fp,40($sp)" << std::endl;
+    out << "\taddiu\t$sp,$sp,48" << std::endl;
+    out << "\tj\t$31" << std::endl;
+    out << "\tnop" << std::endl;
+
+}
+
+inline void DeclarationList::print_asm(std::ofstream& out){
+    if(decl_list!=NULL){
+        decl_list->print_asm(out);
+    }
+    decl->print_asm(out);
 }
 
 inline void Declaration::print_asm(std::ofstream& out){
     decl_spec -> print_asm(out);
-    std::cout << "hi2";
     if(init_declr != NULL){
         init_declr->print_asm(out);
     }
 }
 
 inline void DeclarationSpecifier::print_asm(std::ofstream& out){
-    // if(storage_class_spec != NULL){
-
-    // }
-    // else{
-        type_spec -> print_asm(out);
-        if(decl_spec!=NULL){
-            decl_spec->print_asm(out);
-        }     
-    //}
+    type_spec -> print_asm(out);
+    if(decl_spec!=NULL){
+        decl_spec->print_asm(out);
+    }
 }
 
 inline void TypeSpecifier::print_asm(std::ofstream& out){
@@ -100,12 +163,10 @@ inline void TypeSpecifier::print_asm(std::ofstream& out){
 }
 
 inline void InitDeclarator::print_asm(std::ofstream& out){
-    if(init == NULL){
-        declr->print_asm(out);
-    }
-    else{
-        declr->print_asm(out);
-        /* init->print_asm(out); */
+    declr->print_asm(out);
+
+    if(init != NULL){
+        init->print_asm(out);
     }
     out << std::endl;
 }
@@ -120,12 +181,17 @@ inline void DirectDeclarator::print_asm(std::ofstream& out){
     if(dir_declr != NULL){
         dir_declr->print_asm(out);
     }
+
     if(context.is_GlobalVar == true){
-        *iden = context.GlobalDirectDeclarator;
+        context.GlobalDirectDeclarator = *iden;
+    }
+
+    if(context.in_func == true && iden != NULL){
+        context.FuncName = *iden;
     }
 }
 
- inline void Initializer::print_asm(std::ofstream& out){
+inline void Initializer::print_asm(std::ofstream& out){
     if(init_list != NULL){
         init_list -> print_asm(out);
     }
@@ -154,7 +220,6 @@ inline void AssignmentExpr::print_asm(std::ofstream& out){
     //     ass_op -> print_asm(out);
     //     ass_expr -> print_asm(out);
     // }
-    
 }
 
 inline void UnaryExpr::print_asm(std::ofstream& out){
@@ -174,12 +239,7 @@ inline void UnaryExpr::print_asm(std::ofstream& out){
 
 inline void PostfixExpr::print_asm(std::ofstream& out){
     if(pri_expr != NULL){
-        if(expr == NULL){
-            pri_expr->print_asm(out);
-        }
-        else{
-
-        }
+        pri_expr->print_asm(out);
     }
 }
 
@@ -187,6 +247,9 @@ inline void PrimaryExpr::print_asm(std::ofstream& out){
     if(constant != NULL){
         if(context.is_GlobalVar == true){
             context.GlobalVarNum = stoi(*constant);
+        }
+        if(context.is_return == true){
+            context.returnNum = stoi(*constant);
         }
     }
 }
@@ -242,4 +305,99 @@ inline void MultiplicativeExpr::print_asm(std::ofstream& out){
 
 inline void CastExpr::print_asm(std::ofstream& out){
     un_expr -> print_asm(out);
+}
+
+inline void CompoundStatement::print_asm(std::ofstream& out){
+    if(decl_list != NULL){
+        decl_list -> print_asm(out);
+    }
+    if(state_list != NULL){
+        state_list -> print_asm(out);
+    }
+}
+
+inline void StatementList::print_asm(std::ofstream& out){
+    if(state_list != NULL){
+        state_list -> print_asm(out);
+    }
+    state -> print_asm(out);
+}
+
+inline void Statement::print_asm(std::ofstream& out){
+    if(label_state != NULL){
+        label_state -> print_asm(out);
+    }
+    if(comp_state != NULL){
+        comp_state -> print_asm(out);
+    }
+    if(expr_state != NULL){
+        expr_state -> print_asm(out);
+    }
+    if(select_state != NULL){
+        select_state -> print_asm(out);
+    }
+    if(it_state != NULL){
+        it_state -> print_asm(out);
+    }
+    if(jump_state != NULL){
+        jump_state -> print_asm(out);
+    }
+}
+
+inline void LabeledStatement::print_asm(std::ofstream& out){
+
+}
+
+inline void ExprStatement::print_asm(std::ofstream& out){
+    if(expr != NULL){
+        expr -> print_asm(out);
+    }
+}
+
+inline void SelectionStatement::print_asm(std::ofstream& out){
+    expr -> print_asm(out);
+    if_state -> print_asm(out);
+    if(else_state != NULL){
+        else_state -> print_asm(out);
+    }
+    if(IF != NULL){
+        /*DO SOMETHING HERE */
+    }
+    if(ELSE != NULL){
+        /*DO SOMETHING HERE */
+    }
+    if(IF != NULL){
+        /*DO SOMETHING HERE */
+    }
+}
+
+inline void IterationStatement::print_asm(std::ofstream& out){
+    if(expr != NULL){
+        expr -> print_asm(out);
+    }
+    state -> print_asm(out);
+    if(for_first != NULL){
+        for_first -> print_asm(out);
+    }
+    if(for_second != NULL){
+        for_second -> print_asm(out);
+    }
+    //DO SOMETHING ABOUT TYPE
+    if(add_type != NULL){
+        //DO SOMETHING ABOUT ADD_TYPE
+    }
+}
+
+inline void JumpStatement::print_asm(std::ofstream& out){
+    if(*type == "return"){
+        context.is_return = true;
+        expr -> print_asm(out);
+        out << "\tli\t\t$2," << context.returnNum << std::endl;
+    }
+    //DO SOMETHING ABOUT TYPE
+    
+}
+
+inline void Expr::print_asm(std::ofstream& out){
+    assign_expr -> print_asm(out);
 }
